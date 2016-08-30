ba9213ba6636b22344f964f48feedfbb496ac42b
Magnus Enger <magnus@enger.priv.no>
2016-08-30 09:39:52 +0200

    Add some totals at the bottom of the table
    Also fix a warning about "push to referance".

--- koha-table.pl
+++ koha-table.pl
@@ -55,25 +55,48 @@ foreach my $site ( @plack_sites ) {
 # Table headers
 my $sites = [ [ 'Name', 'Enabled', 'Email', 'SIP2', 'HTTPS', 'Plack', 'Records', 'Items', 'Borrowers', 'Loans' ] ];
 
+my $biblio_total = 0;
+my $items_total = 0;
+my $borrowers_total = 0;
+my $issues_total = 0;
+
 foreach my $site_name ( @all_sites ) {
 
     chomp $site_name;
-    push $sites, [
+
+    my $biblio_count    = sql_count( $site_name, 'biblio' );
+    my $items_count     = sql_count( $site_name, 'items' );
+    my $borrowers_count = sql_count( $site_name, 'borrowers' );
+    my $issues_count    = sql_count( $site_name, 'issues' );
+
+    push @{ $sites }, [
         $site_name, 
         $sites_data->{ $site_name }->{'enabled'},
         $sites_data->{ $site_name }->{'email'},
         $sites_data->{ $site_name }->{'sip2'},
         check_apache( $site_name, 'SSLCertificateFile' ),
         $sites_data->{ $site_name }->{'plack'},
-        sql_count( $site_name, 'biblio' ),
-        sql_count( $site_name, 'items' ),
-        sql_count( $site_name, 'borrowers' ),
-        sql_count( $site_name, 'issues' )
+        $biblio_count,
+        $items_count,
+        $borrowers_count,
+        $issues_count,
     ];
+
+    $biblio_total    += $biblio_count;
+    $items_total     += $items_count;
+    $borrowers_total += $borrowers_count;
+    $issues_total    += $issues_count;
 
 }
 
 say Text::Table::Tiny::table(rows => $sites, header_row => 1);
+
+say "Total sites:   " . scalar @all_sites;
+say "Enabled sites: " . scalar @enabled_sites;
+say "Records:       $biblio_total";
+say "Items:         $items_total";
+say "Borrowers:     $borrowers_total";
+say "Loans:         $issues_total";
 
 sub check_apache {
 
